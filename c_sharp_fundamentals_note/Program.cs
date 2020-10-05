f25a9916396b7e7eaa5956de1b79ff7e5e0dc941
jkchristensen36 <jkchristensen36@gmail.com>
2020-10-05 13:56:15 -0700

    Initial check-in of module c_sharp_fundamentals_note

---
+++ c_sharp_fundamentals_note/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
+        }
+    }
+}
